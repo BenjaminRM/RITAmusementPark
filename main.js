123c738ab4a421282f7e37ed38d2a6590b5bf0c7
BenjaminRM <benjaminrmitch@gmail.com>
2019-02-05 16:30:06 -0500

    test queries v2

--- main.js
+++ main.js
@@ -60,9 +60,4 @@ function testDataConnection() {
 // Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
 
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
-console.log('Testing additional message!');
-console.log('TESTING EB CLI!');
-
 testDataConnection();
